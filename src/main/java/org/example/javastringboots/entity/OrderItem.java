50b47be804af025f57cc5ecda7a4d07d7a32d36e
minhkq3009 <55178535+minhkq3009@users.noreply.github.com>
2024-12-19 11:37:19 +0700

    Add files via upload

---
+++ src/main/java/org/example/javastringboots/entity/OrderItem.java
@@ -0,0 +1,72 @@
+package org.example.javastringboots.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "orders_item")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Double price;
+    @Column(name = "buy_qty")
+    private Long qty;
+
+    @Column(name = "product_id",nullable = false)
+    private Long productId;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id",nullable = false)
+    private Order order;
+
+    @ManyToOne
+    private Product product;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Long getQty() {
+        return qty;
+    }
+
+    public void setQty(Long qty) {
+        this.qty = qty;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
